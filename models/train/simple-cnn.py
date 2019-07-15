0dff41c9dd68f70febe6451425cc6d2d161327a5
Oliver Balfour <leolalor1@gmail.com>
2019-07-15 22:29:35 +1000

    should work simple cnn

--- models/train/simple-cnn.py
+++ models/train/simple-cnn.py
@@ -15,17 +15,17 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 
 # config
-batch_size = 12 # don't have the mem for 15
+batch_size = 10 # don't have the mem for 15
 num_epochs = 3
 train_dir = 'data/proc/'
 model_name = 'models/h5/simple-cnn.h5'
 
 # construct the model
-model_in = Input(shape=(224,224))
-x = Conv2D(32, activation='relu')(model_in)
-x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
-x = Conv2D(64, activation='relu')(x)
-x = MaxPooling2D(pool_size=(2, 2))(x)
+model_in = Input(shape=(224,224,3))
+x = Conv2D(32, kernel_size=(5,5), activation='relu')(model_in)
+x = MaxPooling2D(pool_size=(2,2), strides=(2,2))(x)
+x = Conv2D(64, kernel_size=(5,5), activation='relu')(x)
+x = MaxPooling2D(pool_size=(2,2), strides=(2,2))(x)
 x = Flatten()(x)
 x = Dense(512, activation='relu')(x)
 x = Dense(256, activation='relu')(x)
@@ -34,10 +34,7 @@ model_out = Dense(5, activation='softmax')(x)
 # create the model
 model = Model(inputs=model_in, outputs=model_out)
 
-train_datagen = ImageDataGenerator(
-	preprocessing_function=preprocess_input,
-	validation_split=0.2 # we need 2 generators
-)
+train_datagen = ImageDataGenerator(validation_split=0.2)
 
 train_generator = train_datagen.flow_from_directory(
 	train_dir, target_size=(224,224), batch_size=batch_size,
