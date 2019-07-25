de8d8b8a5a16966c95398f4a903ae7ce34c5c12e
Oliver Balfour <leolalor1@gmail.com>
2019-07-25 20:06:05 +1000

    Minor fixes

--- models/extraction/inception-resnet.py
+++ models/extraction/inception-resnet.py
@@ -9,6 +9,3 @@ from model_utils import process_model
 
 model = InceptionResNetV2(weights='imagenet', include_top=False, pooling='avg')
 process_model(model, 'inception-resnet', 299, preprocess=preprocess_input, max_steps=5000)
-
-# shape is :,7,7,1024 and we want global averages so :,1,1,1024 or equivalently :,1024
-#np.moveaxis(nd,-1,1).mean(axis=-1).mean(axis=-1)
