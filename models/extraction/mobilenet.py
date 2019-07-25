e0cb80f530f38a72ba29ecfd5bc0b85367c45d34
Oliver Balfour <leolalor1@gmail.com>
2019-07-25 19:22:36 +1000

    Added neat mobilenet+nasnet feature extractors and X/Y npy files

--- models/extraction/mobilenet.py
+++ models/extraction/mobilenet.py
@@ -1,55 +1,14 @@
 
-import os
+import os, sys
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras as keras
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications.mobilenet import MobileNet, preprocess_input
+sys.path.append('models')
+from model_utils import process_model
 
-from tensorflow.keras.applications import MobileNet
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.mobilenet import preprocess_input
+model = MobileNet(weights='imagenet', include_top=False, pooling='avg')
+process_model(model, 'mobilenet', 224, preprocess=preprocess_input, max_steps=8000)
 
-model = MobileNet(weights='imagenet', include_top=False)
-
-image_size = 224
-
-train_dir = f'data/proc/{str(image_size)}/'
-batch_size = 1 # must be 1 for the broken image handling to work nicely
-
-def gen_wrapper (gen, cid):
-	while True:
-		try:
-			yield next(gen)
-		except:
-			pass
-
-def process_class (cid):
-	print('Processing cid ' + str(cid))
-
-	data_generator = ImageDataGenerator(
-		rescale=1.0/255,
-		preprocessing_function=preprocess_input
-	)
-	generator_flow = data_generator.flow_from_directory(
-		train_dir, target_size=(image_size,image_size), batch_size=batch_size,
-		class_mode='categorical', shuffle=False, color_mode='rgb',
-		classes=[str(cid)]
-	)
-	generator = gen_wrapper(generator_flow, cid)
-	tensors = model.predict_generator(generator, verbose=1, steps=len(generator_flow), max_queue_size=1)
-	print('Generated tensors for cid ' + str(cid))
-
-	# shape is :,7,7,1024 and we want global averages so :,1,1,1024 or equivalently :,1024
-	vectors = np.moveaxis(tensors, -1,1).mean(axis=-1).mean(axis=-1)
-
-	np.save('data/vectors/mobilenet'+str(cid)+'.npy', vectors)
-	print('Done. Vectors saved')
-
-	del vectors
-	del tensors
-	del generator
-	del generator_flow
-	del data_generator
-
-for cid in range(5):
-	process_class(4-cid) # backwards
+# shape is :,7,7,1024 and we want global averages so :,1,1,1024 or equivalently :,1024
+#np.moveaxis(nd,-1,1).mean(axis=-1).mean(axis=-1)
--- models/model_utils.py
+++ models/model_utils.py
@@ -1,4 +1,66 @@
 
+import numpy as np
+import os
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# takes data/vectors/model/0-4.npy and creates X and Y vectors
+def generate_xy (model):
+	vdir = 'data/vectors/'+model+'/'
+	files = os.listdir(vdir)
+	if 'X.npy' in files: files.remove('X.npy')
+	if 'Y.npy' in files: files.remove('Y.npy')
+	ndarrays = [np.load(vdir+fname) for fname in files]
+	X = np.concatenate(ndarrays)
+	class_ids = [int(fname[0]) for fname in files]
+	Y = np.concatenate([np.zeros((len(ndarrays[cid]), 5)) + cid for cid in class_ids])
+	for i in range(Y.shape[0]):
+		cid = int(Y[i,0])
+		Y[i] = np.zeros(5)
+		Y[i,cid] = 1
+	np.save(vdir+'X.npy', X)
+	np.save(vdir+'Y.npy', Y)
+
+# wraps a generator and ignores errors
+def gen_wrapper (gen):
+	while True:
+		try:
+			yield next(gen)
+		except:
+			pass
+
+# process all classes and generate X, Y
+def process_model (model, modelname, image_size, preprocess=None, postprocess=None, max_steps=5000):
+	for cid in range(5):
+		process_class(model, modelname, 4-cid, image_size, preprocess=preprocess, postprocess=postprocess, max_steps=max_steps)
+	generate_xy(modelname)
+
+# processes a class on a model and saves to data/vectors/model/0-4.npy
+def process_class (model, modelname, cid, image_size, preprocess=None, postprocess=None, max_steps=None):
+	if preprocess is None:
+		preprocess = lambda x: x
+
+	print('Processing cid ' + str(cid))
+
+	data_generator = ImageDataGenerator(
+		rescale=1.0/255,
+		preprocessing_function=preprocess
+	)
+	generator_flow = data_generator.flow_from_directory(
+		f'data/proc/{str(image_size)}/', target_size=(image_size,image_size), batch_size=1, # must be 1 for the broken image handling to work nicely
+		class_mode='categorical', shuffle=True, color_mode='rgb',
+		classes=[str(cid)]
+	)
+	generator = gen_wrapper(generator_flow)
+	tensors = model.predict_generator(
+		generator, verbose=1,
+		steps=min(len(generator_flow), max_steps),
+		max_queue_size=1
+	)
+
+	if postprocess is not None:
+		tensors = postprocess(tensors)
+	np.save('data/vectors/'+modelname+'/'+str(cid)+'.npy', tensors)
+	del tensors
 
 # alphanumerically sorts all files in data/proc/224 and returns the diagnoses for each
 def get_sorted_classes ():
