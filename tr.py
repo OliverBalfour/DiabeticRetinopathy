48a48d22cc59f6890554fa8b9db2a21fe7e32c12
Oliver Balfour <leolalor1@gmail.com>
2019-08-22 21:41:09 +1000

    minor fixes

--- tr.py
+++ tr.py
@@ -2,6 +2,8 @@
 from models.model_utils import load_xy
 from models.stacked.linreg import train
 
+# import all 10 algorithms, train on both vectors, and save acc to txt/csv
+
 model = ['mobilenet', 'densenet121'][1]
 
 X, Y = load_xy(model)
