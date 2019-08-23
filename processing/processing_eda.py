8da1b3729dfd6c8374112c3b71a85dd32c699114
Oliver Balfour <leolalor1@gmail.com>
2019-08-23 10:25:11 +1000

    Working on processing visualisation

--- processing/processing_eda.py
+++ processing/processing_eda.py
@@ -1,39 +1,78 @@
 
 import numpy as np
-import pandas as pd
 import cv2, os
 import matplotlib.pyplot as plt
 
-from dataframes import get_train_df
+from image_functions import pre_process, clahe, add_gaussian, post_process
 
-train_df = get_train_df()
-
-no_samples = 3
-class_names = ['No Retinopathy', 'Mild Nonproliferative', 'Moderate Nonproliferative', 'Severe Nonproliferative', 'Proliferative Retinopathy']
+class_names = ['No Retinopathy', 'Mild', 'Moderate', 'Severe', 'Proliferative']
+stage_names = ['Raw', 'Resized', 'CLAHE', 'Normalised', 'Clipped']
+# class_names = ['Benign', 'Malignant']
 classes = np.arange(len(class_names))
 
-# list of sample id coes by diagnosis in order 012340123... (not 000111... so that plotting is easier)
-samples = np.ndarray.flatten(np.array([
-	train_df.loc[train_df.diagnosis == class_id].sample(no_samples).id_code.tolist() for class_id in classes
-]).T)
+def getmat (m, n):
+	return np.zeros((m, n), dtype='int').tolist()
 
-def draw_samples (images):
-	fig = plt.figure(figsize=(15, 8))
-	for i, id_code in enumerate(samples):
-		ax = fig.add_subplot(no_samples, len(classes), i+1, xticks=[], yticks=[])
-		plt.imshow(images[i]/255)
-		if i // len(classes) == 0:
-			ax.set_title(class_names[i%len(classes)])
+# 2d array of 3d tensors (img sizes may differ)
+def draw_samples (images, ylabels, xlabels, figsize=(12, 9)):
+	fig = plt.figure(figsize=figsize)
+
+	rows = len(images)
+	cols = len(images[0])
+
+	for row in range(rows):
+		for col in range(cols):
+			ax = fig.add_subplot(rows, cols, row * rows + col + 1, xticks=[], yticks=[])
+			plt.imshow(images[row][col] / 255)
+			if row == 0:
+				ax.set_title(xlabels[col])
+			if col == 0:
+				ax.set_ylabel(ylabels[row])
+
+	plt.subplots_adjust(
+		top=0.92,
+		bottom=0.05,
+		left=0.07,
+		right=0.95,
+		hspace=0.3,
+		wspace=0.2
+	)
 	plt.show()
 
-from process import process, post_process
+proc = [
+	lambda read: cv2.cvtColor(cv2.imread(read), cv2.COLOR_BGR2RGB),
+	lambda img: cv2.cvtColor(pre_process(img), cv2.COLOR_RGB2BGR),
+	lambda img: clahe(img),
+	lambda img: add_gaussian(img),
+	lambda img: post_process(img, 224)
+]
 
-# process an image and save it in each specified size
-def process_image (read, size):
-	return post_process(process(cv2.imread(read)), size)
 
-draw_samples([
-	cv2.imread('data/proc/299/'+str(int(train_df.loc[train_df.id_code==id_code].sample().diagnosis))+'/'+id_code+'.png')
-	# process_image(train_df.loc[train_df.id_code==id_code].path.item(), 299)
-	for id_code in samples
-])
+def exemplar_stages ():
+	images = getmat(5,5)
+	fnames = sorted(os.listdir('images/processing-stages'))
+	for row, fname in enumerate(fnames):
+		img = 'images/processing-stages/' + fname
+		for col, fn in enumerate(proc):
+			img = fn(img)
+			images[row][col] = img
+	draw_samples(images, class_names, stage_names)
+
+# broken atm
+def augmentations ():
+	images = getmat(2,4)
+	fnames = sorted(os.listdir('images/augmentations'))
+	for i, fname in enumerate(fnames):
+		img = 'images/augmentations/' + fname
+		images[i % 2][i // 2] = proc[0](img)
+	print(images)
+	draw_samples(images, ['']*2, ['']*4, figsize=((12, 4)))
+
+def misclassified ():
+	pass
+
+def low_quality_stages ():
+	pass
+
+
+augmentations()
