1aae5568b055cfc2b4c2a1f4612a04572823f5fc
Oliver Balfour <leolalor1@gmail.com>
2019-09-03 21:31:31 +1000

    Test set accuracy averaging

--- models/ensemble/average.py
+++ models/ensemble/average.py
@@ -18,14 +18,19 @@ true_labels = outputs['true_labels']['mobilenet']['test']
 
 predictions = np.zeros((test_size,2))
 
-def fn (acc):
-	return max(0, acc - 0.5)
+avg = input('(w)eighted/(u)nweighted: ')
 
-for cnn in outputs:
+def weighted (acc):
+	return max(0, acc - 0.5)
+def unweighted (acc):
+	return 1  / (num_cnns * num_models)
+
+for cnn in ['densenet121', 'mobilenet']:
 	for modelname in outputs[cnn]:
 		model_preds = outputs[cnn][modelname]['test']
-		predictions = predictions + model_preds * fn(outputs['acc'][cnn][modelname])
+		acc = outputs['acc'][cnn][modelname]
+		predictions = predictions + model_preds * (weighted(acc) if avg == 'w' else unweighted(acc))
 
 predictions = np.argmax(predictions, axis=1)
 
-print(metrics.accuracy_score(true_labels, predictions))
+print(metrics.accuracy_score(np.argmax(true_labels, axis=1), predictions))
--- models/ensemble/unweighted_average.py
+++ models/ensemble/unweighted_average.py
@@ -18,11 +18,19 @@ true_labels = outputs['true_labels']['mobilenet']['test']
 
 predictions = np.zeros((test_size,2))
 
-for cnn in outputs:
+avg = input('(w)eighted/(u)nweighted: ')
+
+def weighted (acc):
+	return max(0, acc - 0.6)
+def unweighted (acc):
+	return 1  / (num_cnns * num_models)
+
+for cnn in ['densenet121', 'mobilenet']:
 	for modelname in outputs[cnn]:
 		model_preds = outputs[cnn][modelname]['test']
-		predictions = predictions + model_preds / (num_cnns * num_models)
+		acc = outputs['acc'][cnn][modelname]
+		predictions = predictions + model_preds * (weighted(acc) if avg == 'w' else unweighted(acc))
 
 predictions = np.argmax(predictions, axis=1)
 
-print(metrics.accuracy_score(true_labels, predictions))
+print(metrics.accuracy_score(np.argmax(true_labels, axis=1), predictions))
